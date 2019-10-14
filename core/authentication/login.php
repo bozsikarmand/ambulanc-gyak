58a851a1cff7a2f3b54bc84a5696278240f9f445
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-10-14 09:22:56 +0200

    Debug

--- core/authentication/login.php
+++ core/authentication/login.php
@@ -44,7 +44,7 @@ if (isset($_POST['button-login'])) {
     $run->execute();
     $resultSet = $run -> fetch(PDO::FETCH_ASSOC);
 
-    print_r($resultSet);
+    //print_r($resultSet);
 
     if (!empty($resultSet['le'])) {
         $queryUserPassword = "SELECT szemely.ID as userid, jelszo.ID as pid, jelszo.JelszoHash as ph
@@ -55,9 +55,11 @@ if (isset($_POST['button-login'])) {
         $run->execute();
         $resultSet = $run -> fetch(PDO::FETCH_ASSOC);
 
-        print_r($resultSet);
+       // print_r($resultSet);
 
         if (password_verify($password, $resultSet['ph'])) {
+            echo "OK";
+            /*
             // Beallitok egy munkamenet valtozot amiben eltarolom az email cimet
             $_SESSION["email"] = $queryLoginEmail;
             // Megkeresem azokat akiknel 2 a statusz, es atallitom 3-ra (elso belepes)
@@ -144,7 +146,7 @@ if (isset($_POST['button-login'])) {
         } else {
             $error['emailOrPassDoesNotExist'] = "A megadott email cimmel regisztrált felhasználó nem létezik rendszerünkben vagy a megadott jelszó hibás!";
             echo "A megadott email cimmel regisztrált felhasználó nem létezik rendszerünkben vagy a megadott jelszó hibás!";
-        }
+        }*/
     } else {
         echo "Nem talalom a megadott email cimu felhasznalot!";
     }
