cbd92fa56457ac9d9cb00307814af0e6d011a6e4
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-10-21 11:58:24 +0200

    Utvonal javitasa

--- core/database/populateselect/populateselect.php
+++ core/database/populateselect/populateselect.php
@@ -7,7 +7,7 @@ ini_set("display_errors", "1");
 ini_set("log_errors", 1);
 ini_set("error_log", "/tmp/php-error.log");
 
-require_once ("../../../database/config.php");
+require_once ("../../../core/database/config.php");
 
 $populateSelect = "SELECT Jelleg FROM kozterulet";
 $run = $databaseConnection -> prepare($populateSelect);
