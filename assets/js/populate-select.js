31fd012142637adfddf73bb19ff58fbda95665db
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-10-22 09:50:00 +0200

    let kicserelese var-ra

--- assets/js/populate-select.js
+++ assets/js/populate-select.js
@@ -7,16 +7,16 @@ defaultOption.text = 'Valassz';
 dropdown.add(defaultOption);
 dropdown.selectedIndex = 0;
 
-const url = '../../../assets/data/publicplacetrait.json';
+var  url = '../../../assets/data/publicplacetrait.json';
 
-const request = new XMLHttpRequest();
+var request = new XMLHttpRequest();
 request.open('GET', url, true);
 
 request.onload = function() {
   if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    let option;
-    for (let i = 0; i < data.length; i++) {
+    var data = JSON.parse(request.responseText);
+    var option;
+    for (var i = 0; i < data.length; i++) {
       option = document.createElement('option');
       option.text = data[i].Jelleg;
       option.value = data[i].ID;
