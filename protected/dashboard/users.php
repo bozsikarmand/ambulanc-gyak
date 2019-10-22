a75aaa19d3710c4cb233d3b5d288fc1e45370501
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-10-22 12:11:12 +0200

    OLdalsav javitasa

--- protected/dashboard/users.php
+++ protected/dashboard/users.php
@@ -100,7 +100,7 @@
     </nav>
 
     <div class="row">
-        <div class="col">
+        <div class="col-2">
 
         <div class="mx-0 my-auto d-inline" id="wrapper">
             <div class="bg-dark border-right" id="sidebar-wrapper">        
