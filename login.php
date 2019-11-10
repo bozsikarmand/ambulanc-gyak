30a681c604e08df50c1a59272470e1fc033d0590
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-11-10 11:08:44 +0100

    jelszovisszaallitas link beszurasa a bejelentkezo oldalra

--- login.php
+++ login.php
@@ -31,14 +31,19 @@
             <i class="fas fa-sign-in-alt"></i> Bejelentkezés
         </button>
         <div>
-            <button class="btn btn-lg btn-secondary btn-block" type="submit" form="inner">
+            <button class="btn btn-lg btn-secondary btn-block" type="submit" form="register">
                 <i class="fas fa-users"></i> Segítő leszek!
             </button>
+        </div>
+        <div>
+            <a href="forgotpassword.php">
+                <i class="fas fa-key"></i> Elfelejtetted jelszavad?
+            </a>
         </div>
 
         <p class="mt-5 mb-3 text-muted text-center">&copy; 2019 Ambulánc</p>
     </form>
-    <form id="inner" action="register.php" method="post"></form>
+    <form id="register" action="register.php" method="post"></form>
 
     <script src="assets/js/jquery-3.4.1.min.js"></script>
     <script src="assets/js/popper.min.js"></script>
