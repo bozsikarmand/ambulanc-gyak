4180b45be8b1715a7858b54edfa9c496d3207888
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-11-12 17:49:10 +0100

    SQLlekerdezes javitasa

--- core/admin/list/user.php
+++ core/admin/list/user.php
@@ -3,7 +3,7 @@
 require_once ("../../database/config.php");
 
 $listAvailableUsers = "SELECT 
-                            ID, 
+                            ID as id, 
                             CONCAT(
                                 szemely.Vezeteknev, 
                                 SPACE(1), 
@@ -16,24 +16,38 @@ $listAvailableUsers = "SELECT
 $run = $databaseConnection -> prepare($listAvailableUsers);
 $run->execute();
 
-while ($row = $run->fetch(PDO::FETCH_ASSOC)) { ?>
+?>
 
-<tbody>
+<table class="table" data-toggle="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Név</th>
+            <th scope="col" colspan="2">Műveletek</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php while ($row = $run->fetch(PDO::FETCH_ASSOC)) { ?>
+
     <tr>
-        <th scope="row"><?php echo $row['id'] ?></th>
-        <td><?php echo $row['fullname'] ?></td>
+        <th scope="row">
+            <?php echo $row['id'] ?>
+        </th>
         <td>
-            <a href="edit.php?<?php echo $row['id'] ?>" class="btn btn-warning">
+            <?php echo $row['fullname'] ?>
+        </td>
+        <td>
+            <a href="edit.php" class="btn btn-warning">
                 <i class="fas fa-edit"></i> Módositás
             </a>
         </td>
         <td>
-            <a href="delete.php?<?php echo $row['id'] ?>" class="btn btn-danger">
+            <a href="delete.php" class="btn btn-danger">
                 <i class="fas fa-trash-alt"></i> Törlés
             </a>
         </td>
     </tr>
-</tbody>
 
 <?php } ?>
     
