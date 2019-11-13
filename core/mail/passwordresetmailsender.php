825c84afb1ecb6b6769c368639eb0ed3af5540f4
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-11-13 09:10:32 +0100

    autoload php betoltese

--- core/mail/passwordresetmailsender.php
+++ core/mail/passwordresetmailsender.php
@@ -9,7 +9,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require '../../vendor/autoload.php';
+require_once ("../../vendor/autoload.php");
 
 function sendPasswordReset($loginEmail, $token)
 {
