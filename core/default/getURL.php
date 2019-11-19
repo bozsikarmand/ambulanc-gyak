5cd844a64aa7a7b50030f7ce20ac83ca0ae87161
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-11-19 09:33:56 +0100

    A domain lekerese

--- core/default/getURL.php
+++ core/default/getURL.php
@@ -7,5 +7,5 @@ function getURL() {
   else{
     $protocol = 'http';
   }
-  return $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+  return $protocol . "://" . $_SERVER['HTTP_HOST'];
 }
