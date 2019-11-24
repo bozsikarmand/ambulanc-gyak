c7da902cbe6582a388acbb7fc48e285a51ede6c9
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-11-24 17:04:51 +0100

    Kepfeltoltes javitasa

--- core/userprofile/add/profilepicture.php
+++ core/userprofile/add/profilepicture.php
@@ -11,51 +11,53 @@ $sessionLoginEmail = $_SESSION['email'];
 $stat = 4;
 $newStat = 5;
 
-$image = new Bulletproof\Image($_FILES);
-$image->setName($username)
-      ->setSize(51200, 1500000)
-      ->setMime(array('jpg','png','gif','jpeg'))
-      ->setDimension(1000, 1000)
-      ->setLocation("../../../uploads/avatars", 0777);
-
-if($image["avatar"]){
-	$upload = $image->upload();
+if (isset($_POST['button-user-request-admin-approval'])) {
+	$image = new Bulletproof\Image($_FILES);
+	$image->setName($username)
+		  ->setSize(51200, 1500000)
+		  ->setMime(array('jpg','png','gif','jpeg'))
+		  ->setDimension(1000, 1000)
+		  ->setLocation("../../../uploads/avatars", 0777);
 	
-	if($upload){
-		bulletproof\utils\resize(
-			$image->getFullPath(), 
-			$image->getMime(),
-			$image->getWidth(),
-			$image->getHeight(),
-			20,
-            20
-		);
-		$leadingPart = '../../..'; 
-		$avatarFullPath = $image->getFullPath();
-
-		$avatarNormalizedPath = preg_replace('/^' . preg_quote($leadingPart, '/') . '/', '', $avatarFullPath);
-
-		$addUserAvatar = "UPDATE szemely
-						  JOIN email 
-						  ON email.ID = szemely.ID
-						  SET szemely.ProfilkepUtvonal = :avatarNormalizedPath,
-						      szemely.Statusz = :newStat
-					      WHERE szemely.ProfilkepUtvonal IS NULL
-					      AND szemely.Statusz = :stat
-						  AND email.BelepesiEmail = :sessionloginemail";
+	if($image["avatar"]){
+		$upload = $image->upload();
 		
-		$run = $databaseConnection -> prepare($addUserAvatar);
-		$run->bindValue(':avatarNormalizedPath', $avatarNormalizedPath);
-		$run->bindValue(':newStat', $newStat);
-    	$run->bindValue(':stat', $stat);
-		$run->bindValue(':sessionloginemail', $sessionLoginEmail);
-		$exitcode = $run->execute();
-            
-    	if ($exitcode) {
-        	header('Location: ../../default/frontend/adminapproval.php');
-    	}
-	}
-	else {
-		echo $image->getError();
+		if($upload){
+			bulletproof\utils\resize(
+				$image->getFullPath(), 
+				$image->getMime(),
+				$image->getWidth(),
+				$image->getHeight(),
+				20,
+				20
+			);
+			$leadingPart = '../../..'; 
+			$avatarFullPath = $image->getFullPath();
+	
+			$avatarNormalizedPath = preg_replace('/^' . preg_quote($leadingPart, '/') . '/', '', $avatarFullPath);
+	
+			$addUserAvatar = "UPDATE szemely
+							  JOIN email 
+							  ON email.ID = szemely.ID
+							  SET szemely.ProfilkepUtvonal = :avatarNormalizedPath,
+								  szemely.Statusz = :newStat
+							  WHERE szemely.ProfilkepUtvonal IS NULL
+							  AND szemely.Statusz = :stat
+							  AND email.BelepesiEmail = :sessionloginemail";
+			
+			$run = $databaseConnection -> prepare($addUserAvatar);
+			$run->bindValue(':avatarNormalizedPath', $avatarNormalizedPath);
+			$run->bindValue(':newStat', $newStat);
+			$run->bindValue(':stat', $stat);
+			$run->bindValue(':sessionloginemail', $sessionLoginEmail);
+			$exitcode = $run->execute();
+				
+			if ($exitcode) {
+				header('Location: ../../default/frontend/adminapproval.php');
+			}
+		}
+		else {
+			echo $image->getError();
+		}
 	}
 }
