d2d38fcd82be250ce20a0af5d255c0738af4b04f
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-03 17:53:04 +0100

    jav

--- core/session/create.php
+++ core/session/create.php
@@ -38,7 +38,7 @@ function sessionCreateDatabaseEntry($loginEmail, $databaseConnection) {
     $run->bindValue(':sessionID', $sessionID);
 
     $resultSet = $run->execute();
-    echo $resultSet;
+    return $resultSet;
 }
 
 function sessionCheckPrivilege($loginEmail, $databaseConnection) {
