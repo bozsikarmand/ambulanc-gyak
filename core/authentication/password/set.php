24134dd77194f220deeb04c52792a456c1406d31
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-07 20:10:54 +0100

    debug

--- core/authentication/password/set.php
+++ core/authentication/password/set.php
@@ -5,7 +5,8 @@ session_start();
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/default/getURL.php");
 
-if (isset($_POST['button-password-set'])) {
+if (isset($_GET['token'])) {
+//if (isset($_POST['button-password-set'])) {
     if (empty($_POST['inputPassword'])) {
         $error['inputPassword'] = 'Jelszó megadása kötelező';
     }
@@ -16,11 +17,9 @@ if (isset($_POST['button-password-set'])) {
         $_POST['inputPassword'] !== $_POST['inputPasswordConfirmation']) {
         $error['passwordsDoNotMatch'] = 'A megadott két jelszó nem egyezik!';
     }
-    if (isset($_GET['token'])) {
-        $token = $_GET['token'];
-
-        echo $token;
-    }
+    
+    $token = $_GET['token'];
+    echo $token;
 }
         /*$queryGetToken = "SELECT HitelesitoKod as token 
                           FROM szemely
