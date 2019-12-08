643c677aec99539afeec58e715adbc6e6bd85e7d
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-08 15:34:24 +0100

    #2

--- core/authentication/login.php
+++ core/authentication/login.php
@@ -47,10 +47,11 @@ if (isset($_POST['button-login'])) {
     $run->execute();
     $resultSet = $run -> fetch(PDO::FETCH_ASSOC);
 
-    echo "Email ellenorzes:";
-    print_r($resultSet);
+    //echo "Email ellenorzes:";
+    //print_r($resultSet);
+    // A megfelelo ertek kerul be
 
-    /*if (!empty($resultSet['le'])) {
+    if (!empty($resultSet['le'])) {
         $queryUserPassword = "SELECT szemely.ID as userid, jelszo.ID as pid, jelszo.JelszoHash as ph, email.ID as eid
                               FROM szemely, jelszo, email
                               WHERE szemely.ID = jelszo.ID 
@@ -62,10 +63,10 @@ if (isset($_POST['button-login'])) {
         $run->execute();
         $resultSet = $run -> fetch(PDO::FETCH_ASSOC);
 
-        //echo "Jelszo ellenorzes:";
-        //print_r($resultSet);
+        echo "Jelszo ellenorzes:";
+        print_r($resultSet);
 
-        if (password_verify($password, $resultSet['ph'])) {
+        /*if (password_verify($password, $resultSet['ph'])) {
             // Beallitok egy munkamenet valtozot amiben eltarolom az email cimet
             $_SESSION["email"] = $loginEmail;
             $_SESSION["username"] = getSessionUsername($loginEmail, $databaseConnection);
@@ -157,10 +158,10 @@ if (isset($_POST['button-login'])) {
         } else {
             $error['emailOrPassDoesNotExist'] = "A megadott email cimmel regisztrált felhasználó nem létezik rendszerünkben vagy a megadott jelszó hibás!";
             echo "A megadott email cimmel regisztrált felhasználó nem létezik rendszerünkben vagy a megadott jelszó hibás!";
-        }
+        }*/
     } else {
         echo "Nem talalom a megadott email cimu felhasznalot!";
-    }*/
+    }
 } else {
     echo "Nem nyomtad meg!";
 }
