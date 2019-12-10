fb7988eb58dcd7df3d07d44d3e515148f57af473
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-10 12:20:53 +0100

    datum beillesztes

--- core/authentication/logout.php
+++ core/authentication/logout.php
@@ -4,20 +4,24 @@
 
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 
+    $loginEmail = $_SESSION["email"];
 
+    $endtime = date('Y-m-d H:i:s');
     $active = 1;
     $inactive = 0;
 
-    $endSession = "UPDATE munkamenet
+    $endSession = "UPDATE munkamenet, email
                     SET MunkamenetVege=:endtime,
                         Aktiv=:inactive
                     WHERE MunkamenetVege IS NULL 
-                    AND Aktiv = :active"; 
+                    AND Aktiv = :active
+                    AND BelepesiEmail=:loginemail"; 
 
     $run = $databaseConnection -> prepare($endSession);
     $run->bindValue(':endtime', $endtime);
     $run->bindValue(':inactive', $inactive);
     $run->bindValue(':active', $active);
+    $run->bindValue(':loginemail', $loginEmail);
     $run->execute();
 
     session_unset();
