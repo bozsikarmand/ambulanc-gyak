2301aea5d8c54aa4b077bed450479e99fff31762
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-11 12:05:04 +0100

    sql mod

--- core/userprofile/list/profiledata.php
+++ core/userprofile/list/profiledata.php
@@ -6,19 +6,19 @@ require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 
 function queryUserData($sessionKey, $databaseConnection)
 {
-    $queryUserData = "SELECT email.BelepesiEmail as le,
-                         email.PublikusEmail as pe,
-                         szemely.Felhasznalonev as un, 
-                         szemely.Vezeteknev as fn, 
-                         szemely.Keresztnev as ln, 
-                         szemely.Utonev as mn, 
-                         szemely.VezetekesTel as lt, 
-                         szemely.MobilTel as mt, 
-                         szemely.IRSZ as zip, 
-                         szemely.Varos as city, 
-                         szemely.KozteruletNeve as pn, 
-                         szemely.Hazszam as hn, 
-                         szemely.Epulet as bn, 
+    $queryUserData = "SELECT email.BelepesiEmail,
+                         email.PublikusEmail,
+                         szemely.Felhasznalonev, 
+                         szemely.Vezeteknev, 
+                         szemely.Keresztnev, 
+                         szemely.Utonev, 
+                         szemely.VezetekesTel, 
+                         szemely.MobilTel, 
+                         szemely.IRSZ, 
+                         szemely.Varos, 
+                         szemely.KozteruletNeve, 
+                         szemely.Hazszam, 
+                         szemely.Epulet, 
                   FROM email, szemely, szemelymunkamenet, munkamenet
                   WHERE szemely.ID = email.ID 
                   AND szemely.ID = szemelymunkamant.Szemely.ID 
