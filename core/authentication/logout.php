2d7a29598efdd2c7bee21fa39a24da0022d23ab9
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-13 11:48:26 +0100

    idozona beallitasa

--- core/authentication/logout.php
+++ core/authentication/logout.php
@@ -3,6 +3,7 @@
     session_start();
 
     require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
+    require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/default/timezone.php");
 
     $loginEmail = $_SESSION["email"];
 
