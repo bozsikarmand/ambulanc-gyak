2dadde6708e086452cc2bd0f70212085e463a4c2
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-14 20:26:43 +0100

    jav

--- core/session/expire.php
+++ core/session/expire.php
@@ -1,10 +1,8 @@
 <?php
 
-ini_set('session.cookie_httponly', 1);
-ini_set('session.use_only_cookies', 1);
-
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/default/timezone.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/authentication/utils/logout.php");
 
 session_start();
 
