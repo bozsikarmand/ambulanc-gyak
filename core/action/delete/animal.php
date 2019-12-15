c496aeb76bc77f2107b2d107d8d60c230aadd537
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-15 20:17:47 +0100

    jav

--- core/action/delete/animal.php
+++ core/action/delete/animal.php
@@ -10,40 +10,41 @@ ini_set("error_log", "/tmp/php-error.log");
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 
 $getID = $_GET['id'];
+$getConfirm = $_GET['confirm'];
 
 if (isset($getID)) {
-    if (!empty($_POST['button-delete-animal'])) {
-        $selectAnimal = "SELECT 
-                            Faj, 
-                            HordozoSz, 
-                            HordozoM, 
-                            HordozoH, 
-                            Veszelyes, 
-                            Sulyos, 
-                            EgyedSzam 
-                         FROM allat
-                         WHERE ID = :getid";
+    $selectAnimal = "SELECT 
+                    Faj, 
+                    HordozoSz, 
+                    HordozoM, 
+                    HordozoH, 
+                    Veszelyes, 
+                    Sulyos, 
+                    EgyedSzam 
+                    FROM allat
+                    WHERE ID = :getid";
 
-        $run = $databaseConnection -> prepare($selectAnimal);
+    $run = $databaseConnection -> prepare($selectAnimal);
 
-        $run->bindValue(':getid', $getID);
+    $run->bindValue(':getid', $getID);
         
-        $resultSet = $run->execute();
+    $resultSet = $run->execute();
 
-        if ($resultSet && $_GET['confirm'] == 'yes') {
+    if (isset($getConfirm)) {
+        if ($resultSet && $getConfirm == 'yes') {
             $deleteAnimal = "DELETE 
                              FROM allat
                              WHERE ID=:getid";
-                         
+                             
             $run->bindValue(':getid', $getID);
-
+    
             $resultSet = $run->execute();
-
+    
             if ($resultSet) {
                 header("Location: /protected/dashboard/admin.php");
             }
-        } else if ($resultSet && $_GET['confirm'] == 'no') {
-                header("Location: /protected/dashboard/admin/delete/animal.php");
+        } else if ($resultSet && $getConfirm == 'no') {
+            header("Location: /protected/dashboard/admin/delete/animal.php");
         }
     }
 }
