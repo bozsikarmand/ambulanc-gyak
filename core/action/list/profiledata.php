7d0e451c46d571dbf7d342114ae81b0c9ca09005
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-18 15:11:01 +0100

    lekerdezes jav

--- core/action/list/profiledata.php
+++ core/action/list/profiledata.php
@@ -20,7 +20,7 @@ function listProfileData($databaseConnection, $sessionKey)
                         szemely.Epulet  
                         FROM szemely, szemelymunkamenet, munkamenet
                         WHERE szemely.ID = szemelymunkamenet.SzemelyID 
-                        AND szemelymunkamenet.MunkamenetID = munkamenet.ID 
+                        AND szemelymunkamenet.MunkamenetID = munkamenet.MunkamenetID 
                         AND munkamenet.MunkamenetKulcs=:sessionKey";
 
     $run = $databaseConnection -> prepare($listProfileData);
