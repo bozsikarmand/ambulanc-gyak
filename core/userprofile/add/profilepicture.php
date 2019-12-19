cefb9a4546b143c26028463e6335df3df9585d55
Armand Bozsik <h647222@stud.u-szeged.hu>
2019-12-19 08:08:12 +0100

    debug

--- core/userprofile/add/profilepicture.php
+++ core/userprofile/add/profilepicture.php
@@ -1,7 +1,12 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", "1"); 
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+
 session_start();
-//ob_start();
+ob_start();
 
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/action/get/emailaddress.php");
