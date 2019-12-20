4a34a477d246345806e2b014f0475a775d9cd99b
Bozsik Armand <h647222@stud.u-szeged.hu>
2019-12-20 18:15:22 +0100

    debug

--- protected/dashboard/functions/admin/modify/animal.php
+++ protected/dashboard/functions/admin/modify/animal.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 
+error_reporting(E_ALL);
+ini_set("display_errors", "1"); 
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/action/list/user.php");
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 
