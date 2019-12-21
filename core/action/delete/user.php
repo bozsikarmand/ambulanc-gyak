bb6c2e0cbeb7432048c93bcf01e663223781d1ed
Bozsik Armand <h647222@stud.u-szeged.hu>
2019-12-21 21:45:30 +0100

    jav

--- core/action/delete/user.php
+++ core/action/delete/user.php
@@ -1,27 +1,51 @@
 <?php
 
+session_start();
+
+error_reporting(E_ALL);
+ini_set("display_errors", "1"); 
+ini_set("log_errors", 1);
+ini_set("error_log", "/tmp/php-error.log");
+
 require_once ($_SERVER['DOCUMENT_ROOT'] . "/core/database/config.php");
 
 $getID = $_GET['id'];
+$getConfirm = $_GET['confirm'];
 
-if (!empty($getID)) {
-    $oldStatus = 6;
-    $newStatus = -2;
-    $rejectUser = "UPDATE szemely
-                   SET statusz = :newStatus
-                   WHERE statusz = :oldStatus 
-                   AND ID = :getID";
+if (isset($getID)) {
+    $selectUser = "SELECT 
+                   Statusz
+                   FROM szemely
+                   WHERE ID = :getid";
 
-    $run = $databaseConnection -> prepare($rejectUser);
+    $run = $databaseConnection -> prepare($selectUser);
 
-    $run->bindValue(':newStatus', $newStatus);
-    $run->bindValue(':oldStatus', $oldStatus);
-    $run->bindValue(':getID', $getID);
-
+    $run->bindValue(':getid', $getID);
+        
     $resultSet = $run->execute();
 
-    if ($resultSet) {
-        header("Location: /protected/dashboard/admin.php");
+    if (isset($getConfirm)) {
+        if ($resultSet && $getConfirm == 'yes') {
+            $oldStatus = 6;
+            $newStatus = -2;
+            $deleteUser = "UPDATE szemely
+                        SET statusz = :newStatus
+                        WHERE statusz = :oldStatus 
+                        AND ID = :getID";
+
+            $run = $databaseConnection -> prepare($deleteUser);
+
+            $run->bindValue(':newStatus', $newStatus);
+            $run->bindValue(':oldStatus', $oldStatus);
+            $run->bindValue(':getID', $getID);
+    
+            $resultSet = $run->execute();
+    
+            if ($resultSet) {
+                header("Location: /protected/dashboard/admin.php");
+            }
+        } else if ($resultSet && $getConfirm == 'no') {
+            header("Location: /protected/dashboard/functions/admin/list/user.php");
+        }
     }
 }
-
